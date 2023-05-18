4137c77bbfe5cd2020d16a915b4012032f4910ef
roushanali064 <roushanrhaman064@gmail.com>
2023-05-18 22:34:37 +0600

    banner add done

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import img1 from '../../../assets/slider-1.jpg'
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[600px] rounded-xl">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full rounded-xl" />
+                <div className="carusole-info absolute flex items-center h-full  left-0 right-5 justify-end pr-12">
+                    <div className='text-white space-y-7 w-1/2 pl-12'>
+                        <p className='font-bold text-2xl text-[#000030]'>Your Wow! Store</p>
+                        <h2 className='text-6xl font-bold text-[#E0035D]'>The  Ultimate <br /> Toy Store</h2>
+                        <p className='font-bold text-xl text-[#000030]'>Delivering Smile With Toys. Get Your Unstable Fun And Learning experience With Our Creative Toys </p>
+                        <div className='text-center'>
+                            <button className="btn bg-[#FEBF00] border-none mr-5 px-8 font-bold text-xl text-black rounded-3xl hover:text-white">Read More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide6" className="btn btn-circle bg-[#FEBF00] text-black border-none hover:text-white">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-[#FEBF00] text-black border-none hover:text-white">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,17 @@
 import Main from "../layout/Main";
 
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
         children: [
-            
+            {
+                path: '/',
+                element: <Home />
+            }
         ]
     }
 ])
