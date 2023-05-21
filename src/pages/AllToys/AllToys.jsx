f89a095302e01e03ced95101a2debcc9a76b49ff
roushanali064 <roushanrhaman064@gmail.com>
2023-05-21 23:24:34 +0600

    all done

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -10,10 +10,35 @@ const AllToys = () => {
             .then(res => res.json())
             .then(data => setToys(data))
     }, [])
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const search = form.search.value;
+        console.log(search);
+      
+        fetch(`https://kid-life-server.vercel.app/search?name=${encodeURIComponent(search)}`)
+          .then((res) => res.json())
+          .then((data) => {
+            setToys(data)
+            form.reset()
+        });
+      };
 
 
     return (
         <div className='mt-28 mb-28'>
+            <div className='w-1/3 mb-20 mx-auto'>
+            <form onSubmit={handleSearch}>
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" name='search' placeholder="Search…" className="input input-bordered" />
+                        <button className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            </div>
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
